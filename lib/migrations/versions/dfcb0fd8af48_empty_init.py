a74219611713b66fb71affa2caaf3f4a33cecb50
George <gnm7208@gmail.com>
2025-12-03 09:59:28 +0300

    Alembic implementation

---
+++ lib/migrations/versions/dfcb0fd8af48_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: dfcb0fd8af48
+Revises: 
+Create Date: 2025-12-03 09:56:37.068579
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dfcb0fd8af48'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
